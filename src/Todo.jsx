7155c516b804cd1c62f47fea45f8414183c3ea7e
Mikyas <mikyasengidasew.gmail.com>
2025-10-04 11:47:55 +0300

    Write the html for the app

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -8,78 +8,18 @@ import checkIcon from "./assets/icons/icon-check.svg";
 import checkedIcon from "./assets/icons/icon-checked.svg";
 
 function Todo() {
-  const [todos, setTodos] = useState(() => {
-    const storedTodos = JSON.parse(localStorage.getItem("todos"));
-    return storedTodos ? storedTodos : [];
-  });
-  const [todoInput, setTodoInput] = useState("");
-  const [dateInput, setDateInput] = useState("");
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  function handleTodoInput(event) {
-    setTodoInput(event.target.value);
-  }
-
-  function handleDateInput(event) {
-    setDateInput(event.target.value);
-  }
-
-  function addTodo() {
-    if (!todoInput.trim() && !dateInput.trim()) {
-      alert("Please fill out the forms! Thank you.");
-      return;
-    } else if (!todoInput.trim()) {
-      alert("Please type a task!");
-      return;
-    } else if (!dateInput.trim()) {
-      alert("Please insert a date!");
-      return;
-    }
-
-    const newTask = {
-      id: crypto.randomUUID(),
-      text: todoInput,
-      date: dateInput,
-      completed: false,
-    };
-    setTodos((prev) => [...prev, newTask]);
-    setTodoInput("");
-    setDateInput("");
-  }
-
-  function clearTodos() {
-    setTodos([]);
-  }
-
-  function deleteTodo(id) {
-    const filteredTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(filteredTodos);
-  }
-
-  function checkTodo(id) {
-    const updated = todos.map((todo) =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    );
-    setTodos(updated);
-  }
-
-  console.log(todoInput, dateInput);
-
   return (
-    <div className="h-screen flex pt-20 justify-center text-amber-950">
-      <div className="wrapper bg-amber-500 h-fit p-10 pt-8 rounded-2xl flex flex-col gap-4">
+    <div>
+      <div>
         <header>
-          <nav className="flex items-center gap-4 w-fit">
+          <nav>
             <a href="#">
               <img src={lightNavbarIcon} alt="Home" className="w-[100px]" />
             </a>
-            <h1 className="text-3xl font-semibold">Todo App</h1>
+            <h1>Todo App</h1>
           </nav>
         </header>
-        <main className="flex flex-col items-center gap-5">
+        <main>
           <form
             className="flex gap-x-5 items-center"
             onSubmit={(e) => e.preventDefault()}
@@ -95,9 +35,6 @@ function Todo() {
               id="text-input"
               name="text-input"
               placeholder="e.g, Buy groceries"
-              className="py-1 px-2 border-0 outline-2 rounded-sm focus:outline-amber-800"
-              value={todoInput}
-              onChange={handleTodoInput}
             />
             <label
               htmlFor="date-input"
@@ -105,79 +42,50 @@ function Todo() {
             >
               Insert a date
             </label>
-            <input
-              type="date"
-              name="date-input"
-              id="date-input"
-              className="py-1 px-2 border-0 outline-2 rounded-sm focus:outline-amber-800"
-              value={dateInput}
-              onChange={handleDateInput}
-            />
-            <button
-              type="button"
-              aria-label="Add Task"
-              className="bg-amber-900 px-4 py-1.5 rounded-2xl cursor-pointer"
-              onClick={addTodo}
-              title="Add task"
+            <input type="date" name="date-input" id="date-input" />
+            <label
+              htmlFor="time-input"
+              className="absolute size-[1px] p-0 m-[-1px] overflow-hidden whitespace-wrap border-0"
             >
+              Insert a time period
+            </label>
+            <input type="time" id="time-input" name="time-input" />
+            <button type="button" aria-label="Add Task" title="Add task">
               <img src={addIcon} alt="" />
             </button>
             <button
               type="button"
               aria-label="Clear all tasks"
-              className="bg-amber-900 px-4 py-1.5 rounded-2xl cursor-pointer "
-              onClick={clearTodos}
               title="Clear all tasks"
             >
               <img src={clearIcon} alt="" />
             </button>
           </form>
           <div id="error-message"></div>
-          <ul className="flex flex-col gap-2">
-            {todos.map((todo) => {
-              return (
-                <li
-                  className="flex justify-between items-center text-white font-medium gap-10 bg-amber-700 py-3 px-5 rounded-lg min-w-132"
-                  key={todo.id}
+          <ul>
+            <li>
+              <div>
+                <span>Start Learning TypeScript</span>
+                <span>10/02/2025</span>
+                <span>05:30 AM</span>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  aria-label="Delete task"
+                  title="Delete task"
                 >
-                  <div className="grid">
-                    <span
-                      className={
-                        todo.completed
-                          ? "line-through decoration-2 text-amber-300 decoration-amber-50"
-                          : "text-amber-300"
-                      }
-                    >
-                      {todo.text}
-                    </span>
-                    <span>{todo.date}</span>
-                  </div>
-                  <div className="flex gap-3">
-                    <button
-                      type="button"
-                      aria-label="Delete task"
-                      className="bg-amber-900 px-4 py-1.5 rounded-2xl cursor-pointer"
-                      onClick={() => deleteTodo(todo.id)}
-                      title="Delete task"
-                    >
-                      <img src={deleteIcon} alt="" />
-                    </button>
-                    <button
-                      type="button"
-                      aria-label="Mark task as done"
-                      className="bg-amber-900 px-4 py-1.5 rounded-2xl cursor-pointer"
-                      onClick={() => checkTodo(todo.id)}
-                      title="Mark as done"
-                    >
-                      <img
-                        src={todo.completed ? checkedIcon : checkIcon}
-                        alt=""
-                      />
-                    </button>
-                  </div>
-                </li>
-              );
-            })}
+                  <img src={deleteIcon} alt="" />
+                </button>
+                <button
+                  type="button"
+                  aria-label="Mark task as done"
+                  title="Mark as done"
+                >
+                  <img src={checkIcon} alt="" />
+                </button>
+              </div>
+            </li>
           </ul>
         </main>
       </div>
