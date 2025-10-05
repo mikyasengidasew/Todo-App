ab0fb77b6e8256580de6ddc036e71f2e486cccb6
Mikyas <mikyasengidasew.gmail.com>
2025-10-05 11:27:17 +0300

    Finish the design of the final MVP

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,87 +2,123 @@ import { useState, useEffect } from "react";
 
 import lightNavbarIcon from "../public/favicon-light.png";
 import addIcon from "./assets/icons/icon-add.svg";
+import addIconWhite from "./assets/icons/icon-add-white.svg";
 import clearIcon from "./assets/icons/icon-clear.svg";
+import clearIconWhite from "./assets/icons/icon-clear-white.svg";
 import deleteIcon from "./assets/icons/icon-delete.svg";
+import deleteIconWhite from "./assets/icons/icon-delete-white.svg";
 import checkIcon from "./assets/icons/icon-check.svg";
+import checkIconWhite from "./assets/icons/icon-check-white.svg";
 import checkedIcon from "./assets/icons/icon-checked.svg";
+import checkedIconWhite from "./assets/icons/icon-checked-white.svg";
 
 function Todo() {
   return (
-    <div>
-      <div>
+    <div className="h-svh flex items-center justify-center">
+      <div className="h-fit flex flex-col gap-10">
         <header>
-          <nav>
+          <nav className="flex items-center gap-5">
             <a href="#">
-              <img src={lightNavbarIcon} alt="Home" className="w-[100px]" />
+              <img src={lightNavbarIcon} alt="Home" className="w-[75px]" />
             </a>
-            <h1>Todo App</h1>
+            <h1 className="text-5xl font-bold ">Todo App</h1>
           </nav>
         </header>
-        <main>
+        <main className="grid gap-3">
           <form
-            className="flex gap-x-5 items-center"
+            className="grid gap-5 justify-items-center bg-blue-500 p-5 rounded-lg drop-shadow-xl drop-shadow-blue-500/60"
             onSubmit={(e) => e.preventDefault()}
           >
-            <label
-              htmlFor="text-input"
-              className="absolute size-[1px] p-0 m-[-1px] overflow-hidden whitespace-wrap border-0"
-            >
-              Write a todo
-            </label>
-            <input
-              type="text"
-              id="text-input"
-              name="text-input"
-              placeholder="e.g, Buy groceries"
-            />
-            <label
-              htmlFor="date-input"
-              className="absolute size-[1px] p-0 m-[-1px] overflow-hidden whitespace-wrap border-0"
-            >
-              Insert a date
-            </label>
-            <input type="date" name="date-input" id="date-input" />
-            <label
-              htmlFor="time-input"
-              className="absolute size-[1px] p-0 m-[-1px] overflow-hidden whitespace-wrap border-0"
-            >
-              Insert a time period
-            </label>
-            <input type="time" id="time-input" name="time-input" />
-            <button type="button" aria-label="Add Task" title="Add task">
-              <img src={addIcon} alt="" />
-            </button>
-            <button
-              type="button"
-              aria-label="Clear all tasks"
-              title="Clear all tasks"
-            >
-              <img src={clearIcon} alt="" />
-            </button>
+            <fieldset className="flex gap-5">
+              <label
+                htmlFor="text-input"
+                className="absolute size-[1px] p-0 m-[-1px] overflow-hidden whitespace-wrap border-0"
+              >
+                Write a todo
+              </label>
+              <input
+                className="outline-2 border-0 outline-blue-600 pl-3 p-2 pb-3 rounded-lg text-white
+                hover:outline-3 focus:outline-blue-700 focus:outline-3"
+                type="text"
+                id="text-input"
+                name="text-input"
+                placeholder="e.g, Buy groceries"
+              />
+              <label
+                htmlFor="date-input"
+                className="absolute size-[1px] p-0 m-[-1px] overflow-hidden whitespace-wrap border-0"
+              >
+                Insert a date
+              </label>
+              <input
+                className="outline-2 border-0 outline-blue-600 pl-3 pb-0.5 pr-2 rounded-lg cursor-pointer text-white hover:outline-3 focus:outline-blue-700 focus:outline-3"
+                type="date"
+                name="date-input"
+                id="date-input"
+              />
+              <label
+                htmlFor="time-input"
+                className="absolute size-[1px] p-0 m-[-1px] overflow-hidden whitespace-wrap border-0"
+              >
+                Insert a time period
+              </label>
+              <input
+                className="outline-2 border-0 outline-blue-600 pl-3 pr-2 rounded-lg cursor-pointer text-white hover:outline-3 focus:outline-blue-700 focus:outline-3"
+                type="time"
+                id="time-input"
+                name="time-input"
+              />
+            </fieldset>
+            <fieldset className="w-full flex gap-5">
+              <button
+                className="flex justify-center gap-2 bg-blue-500/90 px-3 pr-6 py-3 rounded-lg font-semibold text-white cursor-pointer drop-shadow-blue-600 drop-shadow-md transition-all ease-in-out duration-400 hover:bg-blue-500 hover:drop-shadow-blue-700 hover:drop-shadow-lg"
+                type="button"
+                aria-label="Add Task"
+                title="Add task"
+              >
+                <img src={addIconWhite} alt="" />
+                <span>Add Task</span>
+              </button>
+              <button
+                className="flex justify-center gap-2 bg-blue-500/90 px-3 pr-4 py-3 rounded-lg font-semibold text-white cursor-pointer drop-shadow-blue-600 drop-shadow-md transition-all ease-in-out duration-400 hover:bg-blue-500 hover:drop-shadow-blue-700 hover:drop-shadow-lg"
+                type="button"
+                aria-label="Clear all tasks"
+                title="Clear all tasks"
+              >
+                <img src={clearIconWhite} alt="" />
+                <span>Clear Tasks</span>
+              </button>
+            </fieldset>
           </form>
           <div id="error-message"></div>
-          <ul>
-            <li>
-              <div>
-                <span>Start Learning TypeScript</span>
-                <span>10/02/2025</span>
-                <span>05:30 AM</span>
+          <ul className="grid gap-3 bg-blue-500 text-white p-3 rounded-lg drop-shadow-xl drop-shadow-blue-300">
+            <li className="flex justify-between items-center p-2 bg-blue-600 rounded-lg cursor-pointer transition-all ease-in-out duration-100 hover:translate-x-[.5px] hover:translate-y-[-.5px]">
+              <div className="grid">
+                <span className="font-semibold text-lg">
+                  Start Learning TypeScript
+                </span>
+                <span className="">10/02/2025</span>
+                <span className="font-medium">
+                  {/* Link this at to the time input field */}
+                  <span className="text-[1.3rem]">@</span> 5:30 AM
+                </span>
               </div>
-              <div>
+              <div className="grid gap-2">
                 <button
+                  className="px-5 py-2 rounded-full bg-red-600/65 cursor-pointer  transition-all ease-in-out duration-200 hover:bg-red-600/80"
                   type="button"
                   aria-label="Delete task"
                   title="Delete task"
                 >
-                  <img src={deleteIcon} alt="" />
+                  <img src={deleteIconWhite} alt="" />
                 </button>
                 <button
+                  className="px-5 py-2 rounded-full bg-blue-700/60 cursor-pointer  transition-all ease-in-out duration-200 hover:bg-blue-700"
                   type="button"
                   aria-label="Mark task as done"
                   title="Mark as done"
                 >
-                  <img src={checkIcon} alt="" />
+                  <img src={checkIconWhite} alt="" />
                 </button>
               </div>
             </li>
